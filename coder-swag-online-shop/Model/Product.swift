ca93e9eff884886cccecf26e1be2e9e8ce91b780
Artur Zarzecki <zarzecki.art@icloud.com>
2021-01-20 07:52:52 +0100

    Set up data

---
+++ coder-swag-online-shop/Model/Product.swift
@@ -0,0 +1,20 @@
+//
+//  Product.swift
+//  coder-swag-online-shop
+//
+//  Created by Artur Zarzecki on 19/01/2021.
+//  Copyright © 2021 Artur Zarzecki. All rights reserved.
+//
+
+import Foundation
+struct Product {
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String,  price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+}
